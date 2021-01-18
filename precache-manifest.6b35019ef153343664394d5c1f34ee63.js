ee4b84c32f6fee20d456c4832b0488b5fec43cbc
claugia13 <ac.delima13@gmail.com>
2021-01-18 13:37:51 -0700

    Updates

---
+++ precache-manifest.6b35019ef153343664394d5c1f34ee63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c211778559f1a8b2b457f986d860df5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70e9cdeee87836acc397",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1e3982f2de3169f35bdd",
+    "url": "/static/js/2.acd7d1e7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.acd7d1e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e9cdeee87836acc397",
+    "url": "/static/js/main.7a37c9bc.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "aecf925f6a67b45ecb010fd2dac9edcd",
+    "url": "/static/media/claudia-de-lima.aecf925f.jpg"
+  },
+  {
+    "revision": "0586a1422fe035b714221649d5942dfe",
+    "url": "/static/media/resume.0586a142.pdf"
+  }
+]);
